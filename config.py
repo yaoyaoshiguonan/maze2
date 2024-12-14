f2e6f63fc0908547220ff44392cf517271822603
yaoyaoshiguonan <yaoyaoguonan@outlook.com>
2024-12-14 21:35:26 +0800

    初始提交

---
+++ config.py
@@ -0,0 +1,7 @@
+
+import pygame
+
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+pygame.init()
---
+++ main.py
@@ -0,0 +1,6 @@
+import pygame
+from pygame.examples.go_over_there import screen
+import config
+
+pygame.init()
+screen = pygame.display.set_mode((1000,600))
